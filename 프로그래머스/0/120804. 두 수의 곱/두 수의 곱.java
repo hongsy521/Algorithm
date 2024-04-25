0d460dce5acd8e166248d408da01ae18b817a1c5
hongsy521 <124027140+hongsy521@users.noreply.github.com>
2024-04-25 11:27:19 +0900

    [level 0] Title: 두 수의 곱, Time: 0.02 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Scanner sc = new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        System.out.println(solution.solution(num1,num2));
+    }
+    public int solution(int num1,int num2){
+        return num1*num2;
+    }
+}
