721a7bb40f65c34ca38b9240eede0c46db9f9f71
hongsy521 <124027140+hongsy521@users.noreply.github.com>
2024-05-09 09:43:35 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+class Solution{
+    public int solution(int n){
+        String s= String.valueOf(n);
+        int answer=0;
+        for(int i=0;i<s.length();i++){
+            answer+=Integer.parseInt(s.substring(i,i+1));
+        }
+        return answer;
+    }
+}
