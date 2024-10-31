4da507dc6dbec10636d32c9a0741a5647b6fc33d
hongsy521 <124027140+hongsy521@users.noreply.github.com>
2024-10-31 22:57:56 +0900

    [level 0] Title: 커피 심부름, Time: 1.26 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for(String o : order){
+            if(o.contains("americano"))
+                answer+=4500;
+            if(o.contains("cafelatte"))
+                answer+=5000;
+            if(o.equals("anything"))
+                answer+=4500;
+        }
+        return answer;
+    }
+}
